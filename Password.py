dc2aeb1559927138ae1ad8c0d4d7beec260c5146
Mankuthimma-lab <59665792+Mankuthimma-lab@users.noreply.github.com>
2020-01-14 16:09:08 +0530

    Update Password.py

--- Password.py
+++ Password.py
@@ -7,28 +7,30 @@ import secret
 
 
 class Password:
-    def hash_password(self, password_string):
-        hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
-        return hashed_password
-
-    def hash_check(self, cleartext_password, hashed_password):
-        if (hmac.compare_digest(bcrypt.hashpw(cleartext_password, hashed_password), hashed_password)):
-            print("Yes")
-        else:
-            print("No")    
-            
-    def pass_salting(num_length=8):
-        return secret.token_bytes(num_length)
-   
-    def password_encoding(password_string,password.encode('utf-8')):
-        enc_password= password_string.encode('utf-8')
-        return enc_password
-    
-    def encrypt_password(password_string,salt)
+    def encrypt_password(self,password_string,salt)
         salted_pass=hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),salt,10000).hex()
         return salted_pass
     
 
+    def hash_check(self, cleartext_password, hashed_password,salt):
+        pasword_enc=encrypt_password(cleartext_password,salt)
+        if (hmac.compare_digest(hashed_password=encrypt_password):
+            print("Yes")
+        else:
+            print("No")    
+            
+    #def pass_salting(num_length=8):
+        #return secret.token_bytes(num_length)
+   
+    #def password_encoding(password_string):
+        #enc_password= password_string.encode('utf-8')
+        #return enc_password
+    
+    
+    
+
 #pw = input("Passwort: ")
 #password = str.encode(pw) #Conversion string to bytes
-
+#def hash_password(self, password_string):
+        #hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
+        #return hashed_password
